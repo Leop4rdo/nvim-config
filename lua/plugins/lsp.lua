69157a024f037ba0518bbf4578e94f94f1dcc4fa
Leonardo Antunes <leonardoantunes@pop-os.localdomain>
2023-02-28 07:52:08 -0300

    idk

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,17 +3,8 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
 lsp.ensure_installed({
-    'tsserver',
-    'eslint',
     'sumneko_lua',
     'rust_analyzer',
-    'cssls',
-    'dockerls',
-    'html',
-    'prismals',
-    'pylsp',
-    'sqlls',
-    'yamlls'
 })
 
 lsp.setup()
